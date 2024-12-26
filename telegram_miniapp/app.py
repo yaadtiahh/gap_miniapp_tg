aa1c2530aadd8d0a733892094f945b4454532b7e
Lleanin <Fifashf@yandex.ru>
2024-12-26 16:15:42 +0100

    откатил изменения до работы бота

--- telegram_miniapp/app.py
+++ telegram_miniapp/app.py
@@ -2,15 +2,13 @@ import os
 from dotenv import load_dotenv
 from telegram import Update, WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler
-import logging
-
 
 # Загружаем переменные окружения
 load_dotenv()
 
 
 async def start(update: Update, context):
-    # Ваш актуальный URL (например, Railway или ngrok)
+    # Ваш актуальный ngrok URL
     web_app_url = "https://d4e9-37-47-130-232.ngrok-free.app/"
 
     # Настраиваем кнопку с Mini App
@@ -28,18 +26,5 @@ bot_token = os.getenv("TG_BOT_TOKEN")
 application = Application.builder().token(bot_token).build()
 application.add_handler(CommandHandler("start", start))
 
-# Настройка Webhook
-webhook_url = f"https://gap_miniapp.up.railway.app/{bot_token}"
-
-logging.basicConfig(level=logging.DEBUG)
-
-# Проверка переменных
-print(f"Webhook URL: {webhook_url}")
-print(f"Port: {os.environ.get('PORT', 8443)}")
-
-
-application.run_webhook(
-    listen="0.0.0.0",
-    port=int(os.environ.get("PORT", 8443)),
-    webhook_url=webhook_url
-)
+# Запуск бота
+application.run_polling()
